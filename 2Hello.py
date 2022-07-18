14f12712576cdd581989d5ee38c901245eb050eb
alex3 <bondar1983ovdoc1@gmail.com>
2022-07-18 12:14:53 +0300

    First version

---
+++ 2Hello.py
@@ -0,0 +1,20 @@
+# sudo apt update
+# sudo reboot
+# sudo shutdown now
+# ls -Al (dir)
+# pwd (echo %cd%)
+# mkdir (mkdir)
+# rmdir, rm ()
+
+
+
+
+# while True:
+    # print('Hello World!')
+print('===========================')
+print('Hello World!')
+
+print('Привет програмист')
+print('2 + 2 = ', 2+2)
+print('10 / 3 = ', 10/3)
+print('===========================')
